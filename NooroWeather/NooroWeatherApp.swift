6572fd13ade35d84ca6439b7492eff34f3ee5912
Matthew Johnson <johnsonmh36@gmail.com>
2024-12-13 12:26:46 -0500

    Initial Commit

---
+++ NooroWeather/NooroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherApp.swift
+//  NooroWeather
+//
+//  Created by Matthew Johnson on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NooroWeatherTests/NooroWeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  NooroWeatherTests.swift
+//  NooroWeatherTests
+//
+//  Created by Matthew Johnson on 12/13/24.
+//
+
+import Testing
+
+struct NooroWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
